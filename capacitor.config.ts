9b5cf87d73dce6ca24d8d59d84db45e69da9b4c1
alanmontgomery <alan.montgomery@tascomi.com>
2021-07-02 23:47:35 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-react-tab-switch',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
